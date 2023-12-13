f160cdeb9761dfe7afc5758358501ff1675c6c86
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-12-13 02:54:18 +0200

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,38 +8,7 @@ The third argument is the file path of the destination
 */
 const fs = require('fs');
 
-// Check if correct number of arguments are provided
-if (process.argv.length !== 5) {
-  console.error('Usage: node 102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
+const data1 = fs.readFileSync(process.argv[2], 'utf8');
+const data2 = fs.readFileSync(process.argv[3], 'utf8');
 
-const [, , fileA, fileB, fileC] = process.argv;
-
-// Read content of fileA
-fs.readFile(fileA, 'utf8', (errA, dataA) => {
-  if (errA) {
-    console.error(`Error reading ${fileA}: ${errA}`);
-    process.exit(1);
-  }
-
-  // Read content of fileB
-  fs.readFile(fileB, 'utf8', (errB, dataB) => {
-    if (errB) {
-      console.error(`Error reading ${fileB}: ${errB}`);
-      process.exit(1);
-    }
-
-    // Concatenate contents of fileA and fileB
-    const concatenatedContent = `${dataA}${dataB}`;
-
-    // Write concatenated content to fileC
-    fs.writeFile(fileC, concatenatedContent, (errWrite) => {
-      if (errWrite) {
-        console.error(`Error writing to ${fileC}: ${errWrite}`);
-        process.exit(1);
-      }
-      console.log(`Concatenated files ${fileA} and ${fileB} into ${fileC}`);
-    });
-  });
-});
+fs.writeFileSync(process.argv[4], data1 + data2);
