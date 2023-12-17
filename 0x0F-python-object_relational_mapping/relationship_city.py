bccd779b7d79581953fee5c446785287279b0648
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-12-18 01:33:37 +0200

    pystyle

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -12,3 +12,4 @@ class City(Base):
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    
