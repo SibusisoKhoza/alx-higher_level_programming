cdc8f53c200a68871aef2f5a7908af37dd7d7750
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-12-18 01:27:12 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Connect to MySQL database
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=db_name)
-    cursor = db.cursor()
-
-    # Retrieve states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
-
-    # Fetch and display results
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-
-    # Close cursor and connection
-    cursor.close()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
