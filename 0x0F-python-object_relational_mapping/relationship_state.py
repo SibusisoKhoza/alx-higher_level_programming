50cf248a45c1132ea65e33a72c9e0fece353df49
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-12-18 01:35:24 +0200

    pystle

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -14,4 +14,3 @@ class State(Base):
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
-    
