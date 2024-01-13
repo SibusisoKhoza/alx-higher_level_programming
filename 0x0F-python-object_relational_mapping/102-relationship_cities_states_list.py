78d8c5af7953145c6c5f0e800ca53687b6e7da58
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-01-13 13:06:53 +0200

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""
-Lists all City objects from the database hbtn_0e_101_usa.
-Usage: ./102-relationship_cities_states_list.py <mysql username>
-<mysql password>
-<database name>
-"""
+# script that lists all City objects from the database hbtn_0e_101_usa
+"""import 'sys','sqlalchemy' & 'relationship_..'."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -12,8 +8,9 @@ from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
