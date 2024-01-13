6700f997b6ec0c24c7db92f3991024e5a5e302cb
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-01-13 13:04:58 +0200

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -13,9 +13,9 @@ from relationship_city import City
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-         sys.argv[1], sys.argv[2], sys.argv[3]),pool_pre_ping=True)
+         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-        
+    
     for city in session.query(City).order_by(City.id):
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
