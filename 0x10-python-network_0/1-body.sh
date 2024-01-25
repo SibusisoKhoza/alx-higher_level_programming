045001f99f1d2462c696b34f0e226efa983528a2
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-01-25 13:30:31 +0200

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+
+# Check if URL is provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Use curl to send a GET request and display the body of the response for a 200 status code
+curl -s -o /dev/null -w "%{http_code}" "$1" | {
+    read -r status
+    if [ "$status" -eq 200 ]; then
+        curl -s "$1"
+    fi
+}
