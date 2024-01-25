dc26b25ded72eae2e70fed1ccc09373846ee7372
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-01-25 13:49:46 +0200

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Use curl to send a GET request and display the body of the response for a 200 status code
-curl -s -o /dev/null -w "%{http_code}" "$1" | {
-    read -r status
-    if [ "$status" -eq 200 ]; then
-        curl -s "$1"
-    fi
-}
+# sends a POST request to the passed URL, and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
